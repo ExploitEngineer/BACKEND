a5d466825382141dfd59c32de2a468d6f8efa588
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-01 09:03:25 +0500

    feat: add backend part3

---
+++ part3/script.js
@@ -0,0 +1,17 @@
+// NPM understanding
+// npm - node package manager
+// node js core mein jo ata hy wo kehlaata hy module & jo npm se download kete hai wo hoty hain packages
+
+// installing and uninstalling anything basics & advanced
+// install - npm install package_name
+// uninstall - npm uninstall package_name
+// installing particular versions - npm install jokes@3.0.9
+
+// understanding node_modules
+
+// dependencies - packages and packages ki dependencies
+
+// devdependencies - asy packages jo sirf development ma kam ayain ga pr jab app ban jay ga or deploy ho jay ga tab hum in packages ko use nhi kr rhy hon ga
+
+// scripts - understanding default scripts PATH and custom scripts
+// kar barr ap koi script chalate ho and us script mein ap likhty ho ye command - npm start | npm run dev | npm run concurrent
