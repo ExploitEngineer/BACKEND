50d6e85361b73355b7a591f65ab44abe7470110d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-17 03:49:44 +0500

    feat: add Backend part7
    I have delted the part7 after coding it and rewrite all the code cause i
    am was facing some issues with code.
    This is the project where you can make files write content in files and
    see your files data & it's just like a database.

---
+++ part7/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function(_, res) {
+  fs.readdir(`./files`, function(_, files) {
+    return res.render("index", { files });
+  });
+});
+
+app.get("/file/:filename", function(req, res) {
+  const { params: { filename } } = req;
+  fs.readFile(`./files/${filename}`, "utf-8", function(_, filedata) {
+    res.render("show", { filename, filedata });
+  });
+});
+
+app.post("/create", function(req, res) {
+  const { body: { title, details } } = req;
+  fs.writeFile(`./files/${title.split(" ").join("")}.txt`, details, function(_) {
+    res.redirect("/");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function() {
+  console.log(`Server is running on port ${PORT}`)
+});
