218627fc944e457d231d099c4af52ff8daabba5b
ExploitEngineer <revilwhoami0098@gmail.com>
2025-06-19 03:08:06 +0500

    feat: add part7 project
    Created the file name change feature and update some code

--- part7/index.js
+++ part7/index.js
@@ -8,28 +8,52 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function(_, res) {
-  fs.readdir(`./files`, function(_, files) {
+app.get("/", function (_, res) {
+  fs.readdir(`./files`, function (_, files) {
     return res.render("index", { files });
   });
 });
 
-app.get("/file/:filename", function(req, res) {
-  const { params: { filename } } = req;
-  fs.readFile(`./files/${filename}`, "utf-8", function(_, filedata) {
+app.get("/file/:filename", function (req, res) {
+  const {
+    params: { filename },
+  } = req;
+  fs.readFile(`./files/${filename}`, "utf-8", function (_, filedata) {
     res.render("show", { filename, filedata });
   });
 });
 
-app.post("/create", function(req, res) {
-  const { body: { title, details } } = req;
-  fs.writeFile(`./files/${title.split(" ").join("")}.txt`, details, function(_) {
+app.get("/edit/:filename", function (req, res) {
+  const {
+    params: { filename },
+  } = req;
+  res.render("edit", { filename });
+});
+
+app.post("/edit", function (req, res) {
+  const {
+    body: { previous, newname },
+  } = req;
+  fs.rename(`./files/${previous}`, `./files/${newname}`, function () {
     res.redirect("/");
   });
 });
 
+app.post("/create", function (req, res) {
+  const {
+    body: { title, details },
+  } = req;
+  fs.writeFile(
+    `./files/${title.split(" ").join("")}.txt`,
+    details,
+    function (_) {
+      res.redirect("/");
+    },
+  );
+});
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
-  console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
 });
